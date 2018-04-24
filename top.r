fccaafe726aded20e1ed5a5465475df7be11f97d
imclark <33160623+imclark@users.noreply.github.com>
2018-04-24 18:51:14 -0500

    Created Simple Networks
    Networks of the top 5 and 10 Reddit posters and the SUbreddits they post to

---
+++ top.r
@@ -0,0 +1,33 @@
+redditors <- data.frame(table(reddit$username))
+
+top_5 <- subset(redditors, Freq>173)
+# AtheismFTW 176
+# cinsere 545
+# spez 858
+# syncretic 931
+# Gangsta_Raper 5608
+
+top_ten <- subset(redditors, Freq>126)
+	
+# darkreef2 128
+# atomichugbot 144
+# pepsi_next 144
+# dummystupid 169
+# Scopolamina 173
+# AtheismFTW 176
+# cinsere 545
+# spez 858
+# syncretic 931
+# Gangsta_Raper 5608
+
+net_5 <- subset(net, reddit.username == top_5$Var1[1] | reddit.username == top_5$Var1[2] |reddit.username == top_5$Var1[3] | reddit.username == top_5$Var1[4] |reddit.username == top_5$Var1[5])
+
+net_10 <- subset(net, reddit.username == top_ten$Var1[1] | reddit.username == top_ten$Var1[2] |reddit.username == top_ten$Var1[3] | reddit.username == top_ten$Var1[4] |reddit.username == top_ten$Var1[5] | reddit.username == top_ten$Var1[6] | reddit.username == top_ten$Var1[7] |reddit.username == top_ten$Var1[8] | reddit.username == top_ten$Var1[9] |reddit.username == top_ten$Var1[10])
+
+top_5_network <- simpleNetwork(net_5)
+
+top_ten_network <- simpleNetwork(net_10)
+
+saveNetwork(top_5_network, file = 'top_5_redditor_network.html')
+
+saveNetwork(top_ten_network, file = 'top_ten_network.html')
